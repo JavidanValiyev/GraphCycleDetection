a8e72b4d6130af2f09fe0d0cec21a4226b586f87
JavidanValiyev <valiyev.javidan@gmail.com>
2025-12-07 02:11:08 +0400

    PUNCH

---
+++ GraphCycleDetection/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//Edges represent connections between graph nodes
+int[,] edges = { { 1, 2 }, { 3, 4},  { 5, 6 }, { 7, 8}, { 1, 3 }, {2,5}, { 5, 7 }, { 6, 8 } };
+var result = DetectCycle(edges);
+Console.WriteLine("Cycle Detected : {0}", result.Item1);
+
+/*Function to detect cycle in a graph using an array*/
+Tuple<bool,int,int> DetectCycle(int[,]edges)
+{
+    int[] graph = new int[edges.Length];
+    //Filling the array with -1. It means it is root
+    Array.Fill(graph,-1);
+     
+    for (int i = 0; i < graph.Length; i++)
+    {
+        var rootOfA = FindRootIndex(graph,edges[i,0]);
+        var rootOfb = FindRootIndex(graph,edges[i,1]);
+        if( rootOfA == rootOfb) return Tuple.Create(true,edges[i,0],edges[i,1]) ;
+        
+        if (graph[edges[i,0]] == -1)
+        {
+            graph[edges[i,1]] = edges[i,0];
+        }   
+    }
+    return Tuple.Create(false,-1,-1);
+}
+
+int FindRootIndex(int[] graph, int index)
+{
+    while (true)
+    {
+        var value = graph[index];
+        if (value == -1) return index;
+        index = value;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # GraphCycleDetection
-Cycle detection in graphs
+Developed a simple cycle detection algorithm.
+I don't suggest that you use it in your project directly due to that i did not test it completely. 
+But you can use it as a reference, and I think it is a good start.
+
+Enjoy!
+
+Source stack: 
+
+[Abdul Bari Disjoint Sets](https://www.sourcestack.com/graph-cycle-detection-algorithm)
+[Geek For Geeks](https://www.geeksforgeeks.org/dsa/graph-and-its-representations/)
